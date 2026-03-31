155c801a239dadbaf389f673e515986f64f6b3be
rpedruzzi1505 <pedruzzirodrigo15@gmail.com>
2026-03-30 21:54:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/helloworld")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": "deu certo"}
